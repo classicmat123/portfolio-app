67985b67851b5ff000733fd6b5337b3d4369ea70
classicmat123 <93411200+classicmat123@users.noreply.github.com>
2022-03-12 17:37:55 -0500

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,3 +2,5 @@
 
 # Stop all servers and start the server as a daemon
 forever stopall
+sudo forever start -c "npm start" /home/ubuntu/myportfolio/ --minUptime 10000 --spinSleepTime 1000
+#npm start
